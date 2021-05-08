1d2798341184f87a1a897ccb7efcd99cce4b4c3f
M-Karamambo <mkaram@eastsideprep.org>
2021-05-08 10:45:32 -0700

    Added responsive design using meta tag

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 var newElement = document.createElement("p");
-newElement.innerHTML = "CONTENTS";
-document.getElementById("Title").appendChild(newElement);
-newElement.style.textAlign = "center";
+// newElement.innerHTML = "CONTENTS";
+// document.getElementById("Title").appendChild(newElement);
+// newElement.style.textAlign = "center";
+$("#Title").css('text-align','center');
+$("#Title").append(newElement);
+var clientWidth = document.documentElement.clientWidth;
+console.log(screen.width + " " + screen.height);
+//$('#Body').css("font-size", "300%"); // Use this when doing the size thing
+var Name = "Not known";
+if (navigator.appVersion.indexOf("Win") != -1) Name = 
+  "Windows OS";
+if (navigator.appVersion.indexOf("Mac") != -1) Name = 
+  "MacOS";
+if (navigator.appVersion.indexOf("X11") != -1) Name = 
+  "UNIX OS";
+if (navigator.appVersion.indexOf("Linux") != -1) Name = 
+  "Linux OS";
